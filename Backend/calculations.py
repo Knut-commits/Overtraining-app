bc43fe717752bbcc764c4ccd1d2561c244b29e61
Knut-commits <26knuto@stu.tes.tp.edu.tw>
2025-09-18 12:16:15 +0800

    modifed my if else ladder to be more efficient

--- Backend/calculations.py
+++ Backend/calculations.py
@@ -1,20 +1,21 @@
 def calculate_score(Heart_rate,sleep,Base_heart_rate):
     
     #calculates the score based on the inputted current retsing heart rate, and other input data and comapares it too the calibration data, whihc is their normal resting heart rate
+    diff = Heart_rate - Base_heart_rate
 
-    if Heart_rate - Base_heart_rate < 5:
+    if diff < 5:
         Hr_score = 0
-
-    if Heart_rate - Base_heart_rate >= 5 and Heart_rate - Base_heart_rate < 10:
+    elif diff < 10:
         Hr_score = 2
-    if Heart_rate - Base_heart_rate >=10 and Heart_rate - Base_heart_rate < 15:
-        Hr_score = 4
-    if  Heart_rate - Base_heart_rate >=15 and Heart_rate - Base_heart_rate < 20:
-        Hr_score = 6
-    if Heart_rate - Base_heart_rate  >= 20 and Heart_rate - Base_heart_rate < 25:
+    elif diff < 15:
+        Hr_score = 5
+    elif diff < 20:
+        Hr_score = 7
+    elif diff < 25:
         Hr_score = 8
-    if Heart_rate - Base_heart_rate >=25:
+    else:
         Hr_score = 9
+    
     #this is deciding the heart rate portion of the overtriang score hwoever it has a maxiumum of 9 meaning only 9 out of the 10 points are can be due to heart rate
 
     Over_training_score = (Hr_score + ((10-sleep)/100)) # so this is my frist draft of the score so it someone were to have a resting heart rate 25 beats over their baseline resting heart rate and had a sleep score of 100 out of 100 they would receive an overtriang score of 10/10
